427f54b6ebcabb74967c6d9faa706fd532f334e7
JB Satrio Nugroho <satrionugroho@BA-000004499JB.local>
2026-08-02 22:09:34 +0700

    Debug serialization

--- SearchService.js
+++ SearchService.js
@@ -2,14 +2,15 @@ const SearchService = {
 
   search(keyword) {
 
-    Logger.log("=== SEARCH SERVICE ===");
-    Logger.log(keyword);
-
     const hasil = ProductRepository.search(keyword);
 
     Logger.log("HASIL = " + hasil.length);
 
-    return hasil;
+    // Tes kirim object yang sangat sederhana
+    return hasil.map(item => ({
+      kode: item.kode,
+      nama: item.nama
+    }));
 
   }
 
